906caca86adf326641160fb3c2354d2c3cc514d5
Cristian Arenas <cristian.arenas@sofka.com.co>
2023-02-09 11:21:09 -0500

    Se creo y refactorizo el escenario y de compra exitosa

--- src/main/java/com/magento/pages/ProcesoDeCompraPage.java
+++ src/main/java/com/magento/pages/ProcesoDeCompraPage.java
@@ -1,7 +1,7 @@
 package com.magento.pages;
 
 import com.magento.common.BasePage;
-import com.magento.model.ClienteModel;
+import com.magento.model.DatosCompraModel;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,35 +10,10 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
 import org.openqa.selenium.support.ui.Select;
-
-
-
 import java.util.List;
-
-
-
 
 public class ProcesoDeCompraPage extends BasePage {
     private static final Logger LOGGER = Logger.getLogger(ProcesoDeCompraPage.class);
-    private static final String CORREO_ELECTRONICO = "pruebasofka@pihey.com";
-    private static final String CONTRASEÑA = "Qwerty12";
-
-
-    @CacheLookup
-    @FindBy(xpath = "/html/body/div[2]/header/div[1]/div/ul/li[2]/a")
-    private WebElement loginLink;
-
-    @CacheLookup
-    @FindBy(id = "email")
-    private WebElement correo;
-
-    @CacheLookup
-    @FindBy(id = "pass")
-    private WebElement contraseña;
-
-    @CacheLookup
-    @FindBy(xpath = "//*[@id=\"send2\"]")
-    private WebElement botonLogin;
 
     @CacheLookup
     @FindBy(id = "ui-id-8")
@@ -46,8 +21,6 @@ public class ProcesoDeCompraPage extends BasePage {
     @CacheLookup
     @FindBy(id = "ui-id-8")
     private WebElement navegarAVentas2;
-
-
 
     @CacheLookup
     @FindBy(xpath = "//*[@id=\"maincontent\"]/div[4]/div[2]/div/div/ul[1]/li[6]/a")
@@ -62,31 +35,50 @@ public class ProcesoDeCompraPage extends BasePage {
     private WebElement botonRealizarCompra;
 
     @CacheLookup
-    @FindBy(id = "HT1W9XA")
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/div[1]/div/input")
+    private WebElement nombre;
+
+    @CacheLookup
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/div[2]/div/input")
+    private WebElement apellido;
+
+    @CacheLookup
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form[1]/fieldset/div/div/input")
+    private WebElement correo;
+    @CacheLookup
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form/div/fieldset/div/div[1]/div/input")
     private WebElement direccion;
 
     @CacheLookup
-    @FindBy(id = "LD3FCX3")
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form/div/div[4]/div/input")
     private WebElement ciudad;
 
     @CacheLookup
-    @FindBy(id = "A99AMDW")
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form/div/div[7]/div/input")
     private WebElement codigoPostal;
 
     @CacheLookup
-    @FindBy(id = "O18C49P")
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[1]/div[2]/form/div/div[9]/div/input")
     private WebElement telefono;
 
     @CacheLookup
     @FindBy(xpath = "/html/body/div[2]/header/div[2]/div[1]/a")
     private WebElement carritoDeCompras;
 
+    @CacheLookup
+    @FindBy(xpath = "//*[@id=\"shipping-method-buttons-container\"]/div/button")
+    private WebElement botonSiguiente;
+    @CacheLookup
+    @FindBy(xpath = "/html/body/div[2]/main/div[2]/div/div[2]/div[4]/ol/li[2]/div/div[3]/form/div[1]/table/tbody/tr[1]/td[1]/input")
+    private WebElement metodosDeEnvio;
 
+    @CacheLookup
+    @FindBy(xpath = "//*[@id=\"checkout-payment-method-load\"]/div/div/div[2]/div[2]/div[4]/div/button")
+    private WebElement botonHacerCompra;
 
-
-
-
-
+    @CacheLookup
+    @FindBy(xpath = "/html/body/div[2]/main/div[1]/h1/span")
+    private WebElement mensajeExitoso;
 
 
 
@@ -95,12 +87,7 @@ public class ProcesoDeCompraPage extends BasePage {
         pageFactoryInitElement(driver,this);
     }
 
-    public void clickEnlogin(){
-        clickEnElemento(loginLink);
-    }
-    public void clickEnBotonLogin(){
-        clickEnElemento(botonLogin);
-    }
+
 
     public void clickEnVentas(){
         clickEnElemento(navegarAVentas);
@@ -111,21 +98,23 @@ public class ProcesoDeCompraPage extends BasePage {
     public void clickChaquetas(){
         clickEnElemento(navegarChaquetas);
     }
+    public void clickSiguiente(){
+        clickEnElemento(botonSiguiente);
+    }
 
     public void clickBotonRealizarCompra(){
         clickEnElemento(carritoDeCompras);
         clickEnElemento(botonRealizarCompra);
     }
+    public void clickMetodoDeEnvio(){
+        clickEnElemento(metodosDeEnvio);
+    }
+    public void clickBotonHacerCompra(){
+        clickJava(botonHacerCompra);
+    }
 
-
-    public void diligenciarLogin(){
-        try{
-            escribirEnCampoTexto(correo,CORREO_ELECTRONICO);
-            escribirEnCampoTexto(contraseña,CONTRASEÑA);
-
-        }catch (Exception exception){
-            LOGGER.error("Error al diligenciar los daos del formulario de login");
-        }
+    public String obtenerMensajeDeTexto(){
+        return obtenerTextoDeElemento(mensajeExitoso);
     }
 
     public void agregarProductosMujer() throws InterruptedException {
@@ -166,7 +155,27 @@ public class ProcesoDeCompraPage extends BasePage {
     public String selecionarEstado(){
         Select listaSelect = new Select(driver.findElement(By.className("select")));
         listaSelect.selectByVisibleText("California");
-//        listaSelect.selectByIndex(12);
         return listaSelect.getFirstSelectedOption().getText();
     }
+
+public void diligenciarFormularioCompra(DatosCompraModel datosCompraModel){
+        try{
+
+            escribirEnCampoTexto(correo,datosCompraModel.getCorreo());
+            escribirEnCampoTexto(nombre,datosCompraModel.getNombre());
+            escribirEnCampoTexto(apellido,datosCompraModel.getApellido());
+            escribirEnCampoTexto(direccion,datosCompraModel.getDireccion());
+            escribirEnCampoTexto(ciudad,datosCompraModel.getCiudad());
+            selecionarEstado();
+            escribirEnCampoTexto(codigoPostal,datosCompraModel.getCodigoPostal());
+            escribirEnCampoTexto(telefono,datosCompraModel.getTelefono());
+            clickMetodoDeEnvio();
+            clickSiguiente();
+            clickBotonHacerCompra();
+        }catch (Exception exception){
+            LOGGER.error("Error llenando los campos de direccion de compra");
+        }
+}
+
+
 }
